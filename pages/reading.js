beebe77c35ee56a817c81c8cde39ed8a968eb52e
Happygeneva <130103639+Happygeneva@users.noreply.github.com>
2025-08-20 13:46:38 +0200

    Create reading.js

---
+++ pages/reading.js
@@ -0,0 +1,8 @@
+export default function Reading() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">📖 リーディング</h1>
+      <p>ここで文章読解の練習ができます。</p>
+    </div>
+  );
+}
