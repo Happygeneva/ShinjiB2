561048445f0dde87c62ca144440f8f2180f350a0
Happygeneva <130103639+Happygeneva@users.noreply.github.com>
2025-08-20 13:47:13 +0200

    Create writing.js

---
+++ pages/writing.js
@@ -0,0 +1,8 @@
+export default function Writing() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">✍️ ライティング</h1>
+      <p>ここで作文の練習ができます。</p>
+    </div>
+  );
+}
